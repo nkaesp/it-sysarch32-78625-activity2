0ca938c42ac1298e4d5df192537344d2da7be693
Nikko Kyle Espina <78625-22206528@SERVER2.DOMAIN>
2024-03-02 16:27:19 +0800

    act2 3/2/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from './Header.jsx'
+import Card from './Card.jsx'
 import Footer from './Footer.jsx'
 
 function App() {
     return(
       <>
         <Header />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Footer />
       </>
     );
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+
+    return(
+        <div className = "card">
+            <img className="card-image" src="#" alt="profile picture"></img>
+            <h2 className="card-title">Miles Maratas</h2>
+            <p className="card-text">milesmaratas29@gmail.com</p>
+        </div>
+    );
+}
+
+export default Card
